abfc68c425e0261e1d387c6b6523f450260e6fe5
JoDe68 <xumepao@yahoo.it>
2025-01-07 19:02:31 +0100

    1

--- lib/views/signup_page.dart
+++ lib/views/signup_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SignupPage extends StatelessWidget {
   const SignupPage({super.key});
@@ -9,42 +10,45 @@ class SignupPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Signup Page'),
       ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.only(left: 32, right: 32),
-          child: Column(
-            children: [
-              Spacer(),
-              Text('Signup Page', style: TextStyle(fontSize: 24)),
-              SizedBox(height: 32),
-              TextField(
-                autofocus: true,
-                autocorrect: false,
-                decoration: InputDecoration(
-                  labelText: 'Email',
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: Get.height,
+          child: Padding(
+            padding: EdgeInsets.only(left: 32, right: 32),
+            child: Column(
+              children: [
+                Spacer(),
+                Text('Signup Page', style: TextStyle(fontSize: 24)),
+                SizedBox(height: 32),
+                TextField(
+                  autofocus: true,
+                  autocorrect: false,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                  ),
                 ),
-              ),
-              SizedBox(height: 32),
-              TextField(
-                autocorrect: false,
-                decoration: InputDecoration(
-                  labelText: 'Password',
+                SizedBox(height: 32),
+                TextField(
+                  autocorrect: false,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                  ),
                 ),
-              ),
-              SizedBox(height: 32),
-              TextField(
-                autocorrect: false,
-                decoration: InputDecoration(
-                  labelText: 'Confirm Password',
+                SizedBox(height: 32),
+                TextField(
+                  autocorrect: false,
+                  decoration: InputDecoration(
+                    labelText: 'Confirm Password',
+                  ),
                 ),
-              ),
-              SizedBox(height: 32),
-              ElevatedButton(
-                onPressed: null,
-                child: Text('Sign up'),
-              ),
-              Spacer(),
-            ],
+                SizedBox(height: 32),
+                ElevatedButton(
+                  onPressed: null,
+                  child: Text('Sign up'),
+                ),
+                Spacer(),
+              ],
+            ),
           ),
         ),
       ),
